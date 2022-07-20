8535a58f7e2fe6ddecc8f750986bb9634ec49ac4
Deo Kumar <40450564+Deokumarjnu@users.noreply.github.com>
2022-07-20 11:04:02 +0530

    Integrated hello lambda with Rest api

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -2,7 +2,14 @@ import { Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs';
 import { Code, Function as LambdaFunction, Runtime } from "aws-cdk-lib/aws-lambda"
 import { join } from 'path';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+
+
+
 export class SpaceStack extends Stack {
+
+    private api = new RestApi(this, 'SpaceApi')
+
     constructor(Scope: Construct, id: string, props: StackProps) {
         super(Scope, id, props);
 
@@ -11,6 +18,12 @@ export class SpaceStack extends Stack {
             code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
             handler: 'hello.main'
         });
+
+        // integrate helloLambda with RestApi
+        const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+        const helloLambdaResourse = this.api.root.addResource('hello');
+        helloLambdaResourse.addMethod('GET', helloLambdaIntegration);
+
     }
 
 
