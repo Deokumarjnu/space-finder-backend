8fea866234266d5d392e91c9dc7f9c0e49beb991
Deo Kumar <40450564+Deokumarjnu@users.noreply.github.com>
2022-07-22 10:15:30 +0530

    Added dynamo db logic

--- services/SpaceTable/create.ts
+++ services/SpaceTable/create.ts
@@ -1,3 +1,31 @@
+import { v4 } from 'uuid';
 import { DynamoDB } from 'aws-sdk';
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 
-const dbClient = new DynamoDB.DocumentClient()
+const dbClient = new DynamoDB.DocumentClient();
+
+async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+
+    // Insert Item
+    const item = {
+        spaceId: v4()
+    }
+
+    const result: APIGatewayProxyResult = {
+        statusCode: 200,
+        body: "Hello from Dynamodb !"
+    }
+
+    try {
+        await dbClient.put({
+            TableName: 'SpaceTable',
+            Item: item
+        }).promise()
+    } catch (error: any) {
+        result.body = error.message;
+        result.statusCode = 500
+    }
+    return result;
+};
+
+export { handler }
