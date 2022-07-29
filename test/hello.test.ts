b9c8bad4d1f889b4d813da42134b41d2b2f725bd
Deo Kumar <40450564+Deokumarjnu@users.noreply.github.com>
2022-07-29 10:58:45 +0530

    just cleanup request http and hello.test

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -3,20 +3,16 @@ import { APIGatewayProxyEvent } from 'aws-lambda';
 // import { handler } from "../services/SpaceTable/create";
 // import { handler } from "../services/SpaceTable/read";
 
-// import { handler } from "../services/SpaceTable/update";
+import { handler } from "../services/SpaceTable/update";
 
-import { handler } from "../services/SpaceTable/delete";
-
-// const event = {
-//     body: {
-//         location: "Sasaram"
-//     }
-// }
-// handler(event as any, {} as any)
+// import { handler } from "../services/SpaceTable/delete";
 
 const event: APIGatewayProxyEvent = {
     queryStringParameters: {
-        "spaceId": "1fd277ae-3778-4967-bb8a-a6059b2718ea"
+        "spaceId": "879ed535-d4fb-418c-8e7d-9cabf5ae5f63"
+    },
+    body: {
+        location: "Updated from test"
     }
 } as any
 
